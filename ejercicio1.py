3eaa5b97dfee89e46420a678294602b90f683ec5
claudiasaavedra <alexasv2015@hotmail>
2020-06-12 11:48:45 -0500

    cuarto commit

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,15 @@
+import logging
+from datetime import datetime, date
+logging.basicConfig(filename='test.log', filemode='w', level=logging.DEBUG)
+
 while True:
     try:
         x = float(input("Ingrese un número:"))
     except ValueError as ex:
         print(str(ex))
+        logging.warning(str(ex), datetime.now().strftime('%d/%m/%Y/%H'))
         print("Ingrese un número por favor.")
-        # raise
     else:
         print("Ingresó un número válido.")
+        logging.info("Fin del programa"+ '' + datetime.now().strftime('%d/%m/%Y/%H'))
         break
